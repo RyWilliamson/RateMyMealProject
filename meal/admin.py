a15f925041f086b72adb7dc196da97212035ff37
RyWilliamson <w.rjwilliamson@gmail.com>
2019-03-01 09:56:15 +0000

    Updated admin interface to support Categories and Recipes

--- meal/admin.py
+++ meal/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from meal.models import Professional, Casual, UserProfile
+from meal.models import Professional, Casual, UserProfile, Category, Recipe
 from django.contrib.auth.admin import UserAdmin
 
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    populated_fields = {'slug': ('name', )}
+
 admin.site.register(UserProfile)
-admin.site.register(Professional,UserAdmin)
-admin.site.register(Casual,UserAdmin)
+admin.site.register(Professional, UserAdmin)
+admin.site.register(Casual, UserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
 
