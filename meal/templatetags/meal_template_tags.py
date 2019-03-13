1eb3195b756e1da23aec073a39041b17be1d8014
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-03-13 12:13:11 +0000

    changes to base and cats - nothing really working

--- meal/templatetags/meal_template_tags.py
+++ meal/templatetags/meal_template_tags.py
@@ -4,5 +4,5 @@ from meal.models import Category
 register = template.Library()
 
 @register.inclusion_tag('meal/cats.html')
-def get_category_list(cat=None):
-    return {'cats':Category.objects.all(), 'act_cat':cat}
+def get_category_list():
+    return {'cats':Category.objects.all()}
