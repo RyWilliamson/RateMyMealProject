8551be27bbbb888d7ebd4f053278221ea3f51c07
RyWilliamson <w.rjwilliamson@gmail.com>
2019-03-15 09:47:02 +0000

    Removed useless template

--- meal/templatetags/meal_template_tags.py
+++ meal/templatetags/meal_template_tags.py
@@ -6,9 +6,3 @@ register = template.Library()
 @register.inclusion_tag('meal/cats.html')
 def get_category_list():
     return {'cats':Category.objects.all()}
-
-@register.inclusion_tag('meal/recipe.html')
-def get_ingredient_list(recipe = None):
-    if (recipe is None):
-        return {'ingredients': []}
-    return {'ingredients': recipe.get_ingredients()}
