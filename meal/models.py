99d27f3485fd1d254d2e7c5c0ea5984eba5d1daa
RyWilliamson <w.rjwilliamson@gmail.com>
2019-03-19 12:31:36 +0000

    Updated models and population script

--- meal/models.py
+++ meal/models.py
@@ -5,50 +5,6 @@ from django.contrib.auth.models import AbstractUser, User
 
 import json
 
-
-class Category(models.Model):
-    name = models.CharField(max_length = 128, unique = True)
-    views = models.IntegerField(default = 0)
-    likes = models.IntegerField(default = 0)
-    slug = models.SlugField(blank = True, unique = True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-
-    def __str__(self):
-        return self.name
-
-class Recipe(models.Model):
-    category = models.ForeignKey(Category)
-    chef = models.ForeignKey(UserProfile)
-    recipe_name = models.TextField()
-    views = models.IntegerField(default = 0)
-    likes = models.IntegerField(default = 0)
-    recipe_ingredients = models.TextField()
-    recipe_directions = models.TextField()
-    image = models.ImageField(upload_to = 'uploaded_images/', default = 'bolognese.jpg')
-    slug = models.SlugField(blank = True, unique = True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.recipe_name)
-        super(Recipe, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.recipe_name
-
-    def get_ingredients(self):
-        return json.decoder.JSONDecoder().decode(self.recipe_ingredients)
-
-    def set_ingredients(self, ingredients):
-        self.recipe_ingredients = json.dumps(ingredients)
-
-    def _get_category_slug():
-        return self.category.slug
-
 class Chef(AbstractUser):
     username = models.CharField(max_length=128, null= True, unique=True)
     email = models.CharField(max_length=128, null= True)
@@ -75,3 +31,45 @@ class Casual(Chef):
             
         )
 
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    slug = models.SlugField(blank = True, unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    category = models.ForeignKey(Category)
+    chef = models.ForeignKey(UserProfile, null = True)
+    recipe_name = models.TextField()
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    recipe_ingredients = models.TextField()
+    recipe_directions = models.TextField()
+    image = models.ImageField(upload_to = 'uploaded_images/', default = 'bolognese.jpg')
+    slug = models.SlugField(blank = True, unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.recipe_name)
+        super(Recipe, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.recipe_name
+
+    def get_ingredients(self):
+        return json.decoder.JSONDecoder().decode(self.recipe_ingredients)
+
+    def set_ingredients(self, ingredients):
+        self.recipe_ingredients = json.dumps(ingredients)
+
+    def _get_category_slug():
+        return self.category.slug
