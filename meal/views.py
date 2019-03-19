7588b64240e461c004dec609aeb7fd69d6238d29
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-03-19 13:00:38 +0000

    updated search functionality

--- meal/views.py
+++ meal/views.py
@@ -235,10 +235,10 @@ def like_recipe(request):
 
 
 def search(request):         
-        query =  request.GET.get('q')
-        results = Recipe.objects.filter(recipe_name__icontains=query)
-        return render(request,"meal/search.html",{"query":query,
-                                                  "results":results})
+	query =  request.GET.get('q')
+	recipeResults = Recipe.objects.filter(recipe_name__icontains=query)
+	categoryResults = Category.objects.filter(name__icontains=query)
+	return render(request,"meal/search.html",{"query":query,"results":recipeResults, "catResults":categoryResults})
 
 
 
