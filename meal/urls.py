af75490cfd69736b2f6c6916ddb7b9e4fbac20ef
RyWilliamson <w.rjwilliamson@gmail.com>
2019-03-20 11:47:36 +0000

    Fixed url's for root and /meal/

--- meal/urls.py
+++ meal/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from meal import views
 
 urlpatterns=[
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.base, name='base'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^registerChef/$', views.registerChef, name='registerChef'),
