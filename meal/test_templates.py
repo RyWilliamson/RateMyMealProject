7a45b80be1b960ca7bdfa48b20dfbf397fcf60d9
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-03-20 23:34:07 +0000

    tried to work on unit tests

---
+++ meal/test_templates.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from meal.models import Category
+from django.core.urlresolvers import reverse
+from meal.models import Category,Recipe
+
+class BaseTemplateTests(TestCase):
+	def setUp(self):
+		
+		
+	def test_home_link_visible_nav_bar(self):
+		
+		
+		
+class loginTests(TestCase):
+	
+	
+	
+
+class logoutTests(TestCase):
+
+
+
+
+class signupTests(TestCase):
+
+
+
+
