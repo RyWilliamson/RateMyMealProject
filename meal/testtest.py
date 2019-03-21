cc061cefb2d3584d533ed7beb5a7dca01b7d4fef
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-03-21 13:44:43 +0000

    removed unnecessary image in media found, more testing stuff

--- meal/testtest.py
+++ meal/testtest.py
@@ -8,12 +8,12 @@ from django.test import Client
 class SignupView(TestCase):
 	def setUp(self):
 		self.client = Client()
-		self.newUser = {'username':"cat", 'email':"cat@gmail.com", 'password':"hello", 'picture':""}
+		self.newUser = {'username':"cat", 'email':"cat@gmail.com", 'password':"hello"}
 
 
 
 	def test_new_user(self):
 		response = self.client.post(reverse('registerRegular'),self.newUser)
-		
-		form = response.context['form']
+		print(response)
+		form = response.context['profile_form']
 		self.assetTrue(form.is_valid())
